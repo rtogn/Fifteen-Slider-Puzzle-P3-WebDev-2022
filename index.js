a770d2b07b890f515ee3eee7779b68b23539d2a8
Rob Tognoni <60898339+rtogn@users.noreply.github.com>
2022-04-13 13:26:54 -0400

    CSS and Refactoring
    Features changes:
       -Added some CSS styling to buttons and page
       -Moves used counter moved to top and adjusted to be one tag instead of two. Added function to set this text. 
       -Shuffle button will say "Start Game" on login or when user selects a new board size, and change to re-shuffle after the first shuffle. 
    
    Code changes:
        -Refactored some JS functions to have more clear names
        -Added header comments to most functions
        -Updated playSound() function to be generic. Takes file path and an optional volume argument. Updated respective code (left music as is though)

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
 (function() {
+    // Globals
     "use strict";
     let NUM = 4;
     let spaceRow = 3;
     let spaceColumn = 3;
     let WIDTH = 100;
-	let moves = 0;
-	let fresh_game = true;
-    window.onload = function(){
+    let moves = 0;
+    let fresh_game = true;
+    window.onload = function() {
         setSize();
-		document.getElementById("shufflebutton").onclick = shuffle;
+        document.getElementById("shufflebutton").onclick = shuffle;
         document.getElementById("select").onchange = changeSize;
         createSquares();
     };
-	
-	function shuffle(){
-		var end_game_prompt = false;
-		if (!fresh_game) {
-			var end_game_prompt = confirm("Are you sure you want to end this game?");
-		}
-		
-		if (fresh_game || end_game_prompt) {
-			for (let j = 0; j < 1000; j++) {
-				let neigbors = [];
-				let allPuzzles = document.getElementsByClassName("puzzletile");
-				for (let i = 0; i < allPuzzles.length; i++) {
-					if (moveable(allPuzzles[i]))
-						neigbors.push(allPuzzles[i]);
-				}
-				let ranNum = getRandomIntInclusive(0, neigbors.length - 1);
-				let tempTop = neigbors[ranNum].style.top;
-				let tempLeft = neigbors[ranNum].style.left;
-				neigbors[ranNum].style.top = spaceRow * WIDTH + "px";
-				neigbors[ranNum].style.left = spaceColumn * WIDTH + "px";
-				neigbors[ranNum].id = "square_" + spaceRow + "_" + spaceColumn;
-				spaceRow = parseInt(tempTop) / WIDTH;
-				spaceColumn = parseInt(tempLeft) / WIDTH;
-			}
-		
-			reset_score();
-		}
+
+    function shuffle() {
+        // Shuffles board (on click)
+        // Prompt if user is in the middle of the game and reset the score on verification. 
+		 document.getElementById("shufflebutton").innerHTML = "Re-Shuffle";
+        var end_game_prompt = false;
+        if (!fresh_game) {
+            var end_game_prompt = confirm("Are you sure you want to end this game?");
+        }
+        if (fresh_game || end_game_prompt) {
+            for (let j = 0; j < 1000; j++) {
+                let neigbors = [];
+                let allPuzzles = document.getElementsByClassName("puzzletile");
+                for (let i = 0; i < allPuzzles.length; i++) {
+                    if (isMovable(allPuzzles[i]))
+                        neigbors.push(allPuzzles[i]);
+                }
+                let ranNum = getRandomIntInclusive(0, neigbors.length - 1);
+                let tempTop = neigbors[ranNum].style.top;
+                let tempLeft = neigbors[ranNum].style.left;
+                neigbors[ranNum].style.top = spaceRow * WIDTH + "px";
+                neigbors[ranNum].style.left = spaceColumn * WIDTH + "px";
+                neigbors[ranNum].id = "square_" + spaceRow + "_" + spaceColumn;
+                spaceRow = parseInt(tempTop) / WIDTH;
+                spaceColumn = parseInt(tempLeft) / WIDTH;
+            }
+            reset_score();
+        }
     }
-	
-	function reset_score() {
-		// Reset music and score. 
-		fresh_game = false;
-		moves = 0;
-		document.getElementById("counter").innerHTML = moves;
-		var audio = document.getElementById("audio");
-		audio.currentTime = 0;
-		audio.volume = 0.05; // Reduce volume. May need to be adjusted if different track used. 
-		audio.play();
-	}
-	
-	
-	function getRandomIntInclusive(min, max) {
+
+    function reset_score() {
+        // Reset music and score when user changes boards. 
+        fresh_game = false;
+        moves = 0;
+        setMoveText();
+        var audio = document.getElementById("audio");
+        audio.currentTime = 0;
+        audio.volume = 0.05; // Reduce volume. May need to be adjusted if different track used. 
+        audio.play();
+    }
+
+    function setMoveText() {
+        // Sets up text that contains win count and counter. 
+        let text = "Moves used: " + moves.toString();
+        document.getElementById("counter").innerHTML = text;
+    }
+
+    function getRandomIntInclusive(min, max) {
+        // Get random integer in given range (inclusive). 
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -64,17 +72,19 @@
         select.id = "select";
         for (var i = 3; i < 7; i++) {
             var option = document.createElement("option");
-            option.innerHTML = i + " * " + i;
+            option.innerHTML = i + " x " + i;
             option.value = i;
             option.id = "option" + i;
             select.appendChild(option);
         }
         document.getElementById("controls").appendChild(select);
         document.getElementById("option4").selected = "selected";
-
+        select.classList.add("buttons");
     }
 
     function changeSize() {
+		// Handles adjusting the board size when user selects an option from the size menu. 
+		document.getElementById("shufflebutton").innerHTML = "Start Game"; // Reset start button when user selects a new size. 
         NUM = this.value;
         spaceRow = this.value - 1;
         spaceColumn = this.value - 1;
@@ -84,10 +94,9 @@
             puzzlearea.removeChild(document.querySelector(".puzzletile"));
         }
         createSquares();
-				
-		var audio = document.getElementById("audio");
-		audio.pause();
-		fresh_game = true;
+        var audio = document.getElementById("audio");
+        audio.pause();
+        fresh_game = true;
     }
 
     function createSquares() {
@@ -110,52 +119,54 @@
         }
     }
 
-	function playsound(){
-		let puzzle_sound = new Audio("win.mp3");
-		puzzle_sound.volume = 0.2;
-		puzzle_sound.play();
-	}
-
-
+    function playSound(sound_location, volume = 0.5) {
+        // Generic method to play sound effects. Pass string to location with optional volume between 0.0 & 1.0
+        let puzzle_sound = new Audio(sound_location);
+        puzzle_sound.volume = volume;
+        puzzle_sound.play();
+    }
 
     function setEvents(div) {
-        div.onmouseover = function () {
-            if (moveable(this)) {
+        // Highlight number in cell on mouseover if tile isMovable.
+        div.onmouseover = function() {
+            if (isMovable(this)) {
                 this.classList.add("highlight");
             }
         };
-
-        div.onmouseout = function () {
-            if (moveable(this)) {
+        // Remove highlight effect when mouse moved off tile. 
+        div.onmouseout = function() {
+            if (isMovable(this)) {
                 this.classList.remove("highlight");
             }
         };
-
-        div.onclick = helper;
+        div.onclick = tileOnClickHandler;
     }
 
-    function helper() {
-        if (moveable(this)) {
+    function tileOnClickHandler() {
+        // Handles tile click event and authenticate board state. 
+        // If tile can be moved: make that move, update scoreboard and validate win state.
+        if (isMovable(this)) {
             makeAMove(this);
-			moves++;
-			document.getElementById("counter").innerHTML = moves;
-            if (win()) {
-				var aLert = document.getElementById("alert");
-				if (aLert.style.display === "none"){
-					aLert.style.display = "block";
-				}
-			document.getElementById("msg").innerHTML = "You win";
-			playsound();
-			var audio = document.getElementById("audio");
-			audio.pause();
+            moves++;
+            setMoveText();
+            if (checkWinState()) {
+                var aLert = document.getElementById("alert");
+                if (aLert.style.display === "none") {
+                    aLert.style.display = "block";
+                }
+                document.getElementById("msg").innerHTML = "You win";
+                playSound("sound/win.mp3", 0.2);
+                var audio = document.getElementById("audio");
+                audio.pause();
+                // Else: Display no change
             } else {
                 document.getElementById("output").innerHTML = "";
             }
         }
     }
-	
 
     function makeAMove(div) {
+        // Moves tile to next available space. 
         div.id = "square_" + spaceRow + "_" + spaceColumn;
         var divRow = parseInt(div.style.top) / WIDTH;
         var divColumn = parseInt(div.style.left) / WIDTH;
@@ -163,12 +174,12 @@
         div.style.left = spaceColumn * WIDTH + "px";
         spaceRow = divRow;
         spaceColumn = divColumn;
-		
-		var tileFx = new Audio("sound/tile_move.mp3");//document.getElementById("tile_move");
-		tileFx.play();
+        // Play sound effect for tile on move.
+        playSound("sound/tile_move.mp3");
     }
 
-    function moveable(div) {
+    function isMovable(div) {
+        // Determines if tile is able to be moved (empty space)
         var divRow = parseInt(div.style.top) / WIDTH;
         var divColumn = parseInt(div.style.left) / WIDTH;
         if (spaceRow == divRow) {
@@ -180,7 +191,8 @@
         }
     }
 
-    function win() {
+    function checkWinState() {
+        // Determines if current state is the win state. 
         var tiles = document.querySelectorAll(".puzzletile");
         for (var i = 0; i < tiles.length; i++) {
             var row = Math.floor(i / NUM);
